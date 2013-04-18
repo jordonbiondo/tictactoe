80b2256c79977ab812a513c861279d987b592ce6
Jordon Biondo <biondoj@mail.gvsu.edu>
2013-04-18 14:05:12 -0400

    Prettier printing

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -18,12 +18,13 @@
   ([player board] (== (count board)
                       (reduce max (map (fn[l] (count-tokens player l)) (board-lists board))))))
 
-(defn get-int [] (try (Integer/parseInt (read-line)) (catch Exception e nil)))
+(defn get-int [name] (print name ": ") (flush)
+  (try (Integer/parseInt (read-line)) (catch Exception e nil)))
 
 (defn valid-input
   ([x y board] (and (>= (min x y)  0) (< (max x y) (count board)) (== (nth (nth board x) y) 0))))
 
-(defn win ([p] (println "Player:" (nth '("O" "X") p) "wins!")))
+(defn win ([p] (println "--- Player" (nth '("X" "O") (dec p)) "wins! ---")))
 
 (defn can-place
   ([x y board] (if (and (>= (min x y) 0) (< (max x y) (count board)))
@@ -36,15 +37,16 @@
 (defn is-cat-game ([board] (not= (reduce min (reduce concat  board)) 0)))
 
 (defn inquire-and-place
-  ([p board] (let [x (get-int) y (get-int)]
+  ([p board] (println "It is " (nth '("X" "O") (dec p)) "'s turn") (flush)
+     (let [x (get-int "X") y (get-int "Y")]
                (if (and x y (valid-input x y board)) (place p x y board)
-                   (do (println "Invalid Input") (inquire-and-place p board))))))
+                   (do (println "--- Invalid Input ---") (inquire-and-place p board))))))
 
 (defn game-step [p1 p2 board]
   (let [afterp1 (inquire-and-place p1 board)]
     (if (player-win p1 (dump-board afterp1)) (win p1)
-        (if (is-cat-game afterp1) (println "Cat's game") (game-step p2 p1 afterp1)))))
+        (if (is-cat-game afterp1) (println "--- Cat's game--- ") (game-step p2 p1 afterp1)))))
 
 (defn -main [& args]
-  (println "Welcome to tic tac toe!\nEnter (x y) coords of placement")
+  (println "Welcome to tic tac toe!\nEnter x y coords lines for token placement")
   (game-step 1 2 (make-board)))
